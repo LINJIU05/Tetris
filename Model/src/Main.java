6b141d3f3cbcf5e005da0fb6f253700592197461
LINJIU05 <2998119975@qq.com>
2024-11-14 22:30:56 +0800

    Update Main.java

--- Model/src/Main.java
+++ Model/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int count=1;
+        if(count<5){
+            System.out.println("Hello world!");
+            count++;
     }
 }
